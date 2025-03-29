6002673b617211b506727e5d0e15cb363fa98b05
Martin Filkov <68849535+martinfilkov@users.noreply.github.com>
2025-03-29 18:13:18 +0200

    [MZ-7] Create Mobile and Brand entities

---
+++ backend/src/main/java/com/tuvarna/mobilezone/persistence/entity/Mobile.java
@@ -0,0 +1,48 @@
+package com.tuvarna.mobilezone.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@Table(name = "mobiles")
+public class Mobile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id", nullable = false)
+    private Brand brand;
+
+    @Column(name = "model", nullable = false, unique = true)
+    private String model;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "operationSystem", nullable = false)
+    private String operatingSystem;
+
+    @Column(name = "ram", nullable = false)
+    private Integer ram;
+
+    @Column(name = "storage", nullable = false)
+    private Integer storage;
+
+    @Column(name = "battery", nullable = false)
+    private Double screenSize;
+
+    @Column(name = "camera", nullable = false)
+    private String processor;
+
+    @Column(name = "screenSize", nullable = false)
+    private String resolution;
+}
