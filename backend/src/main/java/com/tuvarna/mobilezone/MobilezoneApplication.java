3789e07e5a8170c1318ed774d58a5cbafb7d2784
martinfilkov <martinfilkov1@gmail.com>
2025-03-29 16:53:35 +0200

    backend setup

---
+++ backend/src/main/java/com/tuvarna/mobilezone/MobilezoneApplication.java
@@ -0,0 +1,13 @@
+package com.tuvarna.mobilezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilezoneApplication.class, args);
+	}
+
+}
